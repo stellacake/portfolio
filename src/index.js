93c1a7dd03bb98e088b0407dd499da6aedc23937
Stella <stella.dmt@gmail.com>
2020-12-07 15:38:07 +0100

    fixed scroll

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import ScrollToTop from "./components/1-Global-components/ScrollToTop";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<HashRouter basename="/">
+			<ScrollToTop />
 			<App />
 		</HashRouter>
 	</React.StrictMode>,
